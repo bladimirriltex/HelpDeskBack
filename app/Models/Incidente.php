74b40326e192c1e1c3aa25c8c274b3d9b360114e
Alejandro Figueroa <alejandrdx@gmail.com>
2021-06-29 22:11:21 -0500

    sdaasda

--- app/Models/Incidente.php
+++ app/Models/Incidente.php
@@ -14,7 +14,7 @@ class Incidente extends Model
     protected $primaryKey = 'id';
 
     protected $fillable = [
-        'Nombresdas',
+        'Nombres',
         'Fecha_Fin',
         'Descripcion',
         'Respuesta',
